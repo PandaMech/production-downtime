e86bc934240249be817ae9a5dadcac7c002c9bf3
adnapa <vinu_mji_mech@hotmail.com>
2023-03-30 21:19:29 +1100

    init

---
+++ main.js
@@ -0,0 +1,5 @@
+
+function button_pressed(){
+    document.getElementById("test_label").innerHTML = "Pressed!"
+    document.getElementById("sample_text").value = document.getElementById("omori_date").value
+}
